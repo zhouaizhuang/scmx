f695f85092c6d046c11e5ad3ed6b7a242b535c91
周爱桩 <840043348@qq.com>
2021-10-23 19:11:07 +0800

    首页获取信息

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,6 @@
 import { showToast } from "../../common";
 import { getLocation, address2LatLng } from "../../tencentPosition"
+import { post } from "../../libs/network"
 let plugin = requirePlugin('routePlan');
 const { QQMAP_KEY } = require('../../config/index')
 // 获取应用实例
@@ -12,64 +13,70 @@ Page({
     clickPointItem: {}, // 选择的点位
     isShowDetail: false, // 是否显示详情
     markers: [
-      { // 绘制浮标，传入JSON支持多个
-        iconPath: 'http://r0stwq2aa.bkt.clouddn.com/xcx/scmx_has_mi.png', //浮标图片路径，推荐png图片
-        id: 1, // Id支持多个，方便后期点击浮标获取相关信息
-        latitude: 31.83052, // 经度
-        longitude: 119.97147, //纬度
-        width: 30,
-        height:38
-      },
-      {
-        iconPath: 'http://r0stwq2aa.bkt.clouddn.com/xcx/scmx_has_mi.png', //浮标图片路径，推荐png图片
-        id: 2, // Id支持多个，方便后期点击浮标获取相关信息
-        latitude: 31.83055, // 经度
-        longitude: 119.97157, //纬度
-        width: 30,
-        height:38
-      },
-      { // 绘制浮标，传入JSON支持多个
-        iconPath: 'http://r0stwq2aa.bkt.clouddn.com/xcx/scmx_has_mi.png', //浮标图片路径，推荐png图片
-        id: 3, // Id支持多个，方便后期点击浮标获取相关信息
-        latitude: 31.83155, // 经度
-        longitude: 119.97157, //纬度
-        width: 30,
-        height:38
-      },
-      {
-        iconPath: 'http://r0stwq2aa.bkt.clouddn.com/xcx/scmx_has_mi.png', //浮标图片路径，推荐png图片
-        id: 4, // Id支持多个，方便后期点击浮标获取相关信息
-        latitude: 31.83152, // 经度
-        longitude: 119.97257, //纬度
-        width: 30,
-        height:38
-      },
-      {
-        iconPath: 'http://r0stwq2aa.bkt.clouddn.com/xcx/scmx_has_mi.png', //浮标图片路径，推荐png图片
-        id: 5, // Id支持多个，方便后期点击浮标获取相关信息
-        latitude: 31.831022, // 经度
-        longitude: 119.97259, //纬度
-        width: 30,
-        height:38
-      },
-      {
-        iconPath: 'http://r0stwq2aa.bkt.clouddn.com/xcx/scmx_has_mi.png', //浮标图片路径，推荐png图片
-        id: 6, // Id支持多个，方便后期点击浮标获取相关信息
-        latitude: 31.830522, // 经度
-        longitude: 119.97259, //纬度
-        width: 30,
-        height:38
-      }
+      // { // 绘制浮标，传入JSON支持多个
+      //   iconPath: 'http://r0stwq2aa.bkt.clouddn.com/xcx/scmx_has_mi.png', //浮标图片路径，推荐png图片
+      //   id: 1, // Id支持多个，方便后期点击浮标获取相关信息
+      //   latitude: 31.83052, // 经度
+      //   longitude: 119.97147, //纬度
+      //   width: 30,
+      //   height:38
+      // },
+      // {
+      //   iconPath: 'http://r0stwq2aa.bkt.clouddn.com/xcx/scmx_has_mi.png', //浮标图片路径，推荐png图片
+      //   id: 2, // Id支持多个，方便后期点击浮标获取相关信息
+      //   latitude: 31.83055, // 经度
+      //   longitude: 119.97157, //纬度
+      //   width: 30,
+      //   height:38
+      // },
+      // { // 绘制浮标，传入JSON支持多个
+      //   iconPath: 'http://r0stwq2aa.bkt.clouddn.com/xcx/scmx_has_mi.png', //浮标图片路径，推荐png图片
+      //   id: 3, // Id支持多个，方便后期点击浮标获取相关信息
+      //   latitude: 31.83155, // 经度
+      //   longitude: 119.97157, //纬度
+      //   width: 30,
+      //   height:38
+      // },
+      // {
+      //   iconPath: 'http://r0stwq2aa.bkt.clouddn.com/xcx/scmx_has_mi.png', //浮标图片路径，推荐png图片
+      //   id: 4, // Id支持多个，方便后期点击浮标获取相关信息
+      //   latitude: 31.83152, // 经度
+      //   longitude: 119.97257, //纬度
+      //   width: 30,
+      //   height:38
+      // },
+      // {
+      //   iconPath: 'http://r0stwq2aa.bkt.clouddn.com/xcx/scmx_has_mi.png', //浮标图片路径，推荐png图片
+      //   id: 5, // Id支持多个，方便后期点击浮标获取相关信息
+      //   latitude: 31.831022, // 经度
+      //   longitude: 119.97259, //纬度
+      //   width: 30,
+      //   height:38
+      // },
+      // {
+      //   iconPath: 'http://r0stwq2aa.bkt.clouddn.com/xcx/scmx_has_mi.png', //浮标图片路径，推荐png图片
+      //   id: 6, // Id支持多个，方便后期点击浮标获取相关信息
+      //   latitude: 31.830522, // 经度
+      //   longitude: 119.97259, //纬度
+      //   width: 30,
+      //   height:38
+      // }
     ]
   },
   mapCtx: null,
   // 点击标记点的回调
-  markertap(e) { // 这是一个事件，在wxml中绑定这个事件，点击浮标后
+  async markertap(e) { // 这是一个事件，在wxml中绑定这个事件，点击浮标后
     // console.log(e)
     const markerId = e.markerId || e.detail.markerId
-    console.log(markerId)
+    const checkedItem = this.data.markers.find(item => item.id == markerId) || {}
+    const clickPointItem = await post('/wap/machine/info', {
+      machine_id: checkedItem['machine_id']
+    })
     setTimeout(() => {
-      this.setData({ isShowDetail: true, clickPointItem: {id: markerId, longitude: 119.97257, latitude: 31.83155, name: '常州市市政府1号', address:'详细地址128-1'} })
+      this.setData({
+        isShowDetail: true,
+        clickPointItem
+      })
     }, 200)
     // const checkedPoint = markers.find(item => item.id == markerId)
     // 根据id调借口查出详情数据
@@ -103,14 +110,35 @@ Page({
   },
   // 开始导航
   openLocation(){
-    const {longitude, latitude, name,address} = this.data.clickPointItem
-    console.log(longitude, latitude)
+    const { map_lat, map_lng, name, address } = this.data.clickPointItem
     wx.openLocation({ //此设置属于高级APi,可以打开微信内置地图组件
-      latitude,
-      longitude,
+      latitude: Number(map_lat),
+      longitude: Number(map_lng),
       name,
       address
     })
+  },
+  goPhone(){
+    wx.makePhoneCall({
+      phoneNumber: '18888179152' //仅为示例，并非真实的电话号码
+    })
+  },
+  async getMarks(){
+    let markArr = await post('/wap/machine/list')
+    markArr = markArr.map(v => {
+      const { id, map_lat, map_lng } = v
+      console.log(map_lat, map_lng)
+      v.id = Number(v.id)
+      return {
+        iconPath: 'http://r0stwq2aa.bkt.clouddn.com/xcx/scmx_has_mi.png', //浮标图片路径，推荐png图片
+        latitude: Number(map_lat), // 经度
+        longitude: Number(map_lng), //纬度
+        width: 30,
+        height:38,
+        ...v
+      }
+    })
+    this.setData({markers:markArr})
   },
   async onLoad() {
     this.mapCtx = wx.createMapContext('myMap')
@@ -119,41 +147,6 @@ Page({
     // const { lat, lng } = await address2LatLng('常州化龙网络科技股份有限公司')
     this.setData({lat, lng})
     console.log(lat, lng)
+    this.getMarks()
   },
-  async goMap(){
-    // wx.getLocation({
-    //   type: 'gcj02', //返回可以用于wx.openLocation的经纬度
-    //   success (res) {
-    //     const latitude = res.latitude
-    //     const longitude = res.longitude
-    //     wx.openLocation({
-    //       latitude,
-    //       longitude,
-    //       address: '常州化龙网络科技股份有限公司',
-    //       scale: 18
-    //     })
-    //   }
-    // })
-    // const { lat, lng } = await address2LatLng('常州化龙网络科技股份有限公司')
-    // wx.openLocation({
-    //   latitude: 31.812061 ,
-    //   longitude: 119.994034,
-    //   // address: '常州化龙网络科技股份有限公司',
-    //   scale: 18
-    // })
-    let referer = '一潞有你'; //调用插件的app的名称
-    let endPoint = JSON.stringify({ //终点
-      'name': '吉野家(北京西站北口店)',
-      'latitude': 39.89631551,
-      'longitude': 116.323459711
-    });
-    wx.navigateTo({
-      url: 'plugin://routePlan/index?key=' + QQMAP_KEY + '&referer=' + referer + '&endPoint=' + endPoint
-    });
-  },
-  goPhone(){
-    wx.makePhoneCall({
-      phoneNumber: '18888179152' //仅为示例，并非真实的电话号码
-    })
-  }
 })
